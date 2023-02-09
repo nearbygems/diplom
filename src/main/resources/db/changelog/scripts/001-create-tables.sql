bf496d5d1e55ce67aa185911abd405646655eb6e
bergen <Bergen.Asym@kaspi.kz>
2023-02-09 20:17:11 +0600

    added indexes

--- src/main/resources/db/changelog/scripts/001-create-tables.sql
+++ src/main/resources/db/changelog/scripts/001-create-tables.sql
@@ -1,11 +1,11 @@
-CREATE TABLE company
+CREATE TABLE IF NOT EXISTS company
 (
     "id"   BIGINT PRIMARY KEY,
     "name" VARCHAR(256) NOT NULL,
     "city" VARCHAR(256) NOT NULL
 );
 
-CREATE TABLE vacancy
+CREATE TABLE IF NOT EXISTS vacancy
 (
     "id"           BIGINT PRIMARY KEY,
     "title"        VARCHAR(256) NOT NULL,
---
+++ src/main/resources/db/changelog/scripts/002-create-indexes.sql
@@ -0,0 +1,1 @@
+CREATE INDEX IF NOT EXISTS vacancy_fkey ON vacancy (company_id)
